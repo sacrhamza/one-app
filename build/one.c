5bcfc4952ef77904347747a50cd9569bcd379102
sacrhamza <sacrhamzaschool@gmail.com>
2025-04-11 21:24:20 +0100

    impliment mouse wheel and add logos

--- build/one.c
+++ build/one.c
@@ -38,6 +38,17 @@ Color	button_state_color(t_state state)
 	return (button_colors[state]);
 }
 
+
+int	count_packages(char **str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
 int main()
 {
 
@@ -51,19 +62,20 @@ int main()
 	Rectangle	box;
 	int		parent_box_size = 64;
 
-	app_size = 6;
-	char *str[] = {"nvim", "ghostty", "vivaldi", "firefox", "zen", "firefox-developer", NULL};
+	char *str[] = {"nvim", "ghostty", "vivaldi", "firefox", "zen", "firefox-developer", "ffmpeg", "thunderbird", "tor", "opera" ,NULL};
+	app_size = count_packages(str);
 	count = 0;
 	screen_dimention = (t_screen_dimention){800, 800};
 	goal = 0;
 	i = 0;
 	j = 0;
+	int	mouse_wheel = 0;
 	Color color;
 	box = (Rectangle){.width = 100, .height = 35};
 
 	Texture2D	sprite [app_size];
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);    // Window configuration flags
-	InitWindow(screen_dimention.width, screen_dimention.height, "rectangle");
+	InitWindow(screen_dimention.width, screen_dimention.height, "one");
 	SetTargetFPS(60);
 
 	//load logos for every package
@@ -79,36 +91,37 @@ int main()
 		BeginDrawing();
 		ClearBackground((Color){68,71,90, 255});
 
-		if (IsKeyPressed('Q'))
+		if (IsKeyPressed(KEY_Q))
 			break ;
 
 		i = 0;
 		if (screen_dimention.width > MAX_SCREEN_WIDTH && screen_dimention.height > MAX_SCREEN_WIDTH)
 		{
+			if ((mouse_wheel <= 0 && GetMouseWheelMove() > 0) || mouse_wheel <= -10)
+				mouse_wheel -= GetMouseWheelMove() * 25;
 			while (i < app_size)
 			{
-				if (CheckCollisionPointRec(GetMousePosition(), (Rectangle){screen_dimention.width - box.width - 10, i * (parent_box_size + 30) + 10 + ((parent_box_size - box.height) / 2), box.width, box.height}))
+				if (CheckCollisionPointRec(GetMousePosition(), (Rectangle){screen_dimention.width - box.width - 10, i * (parent_box_size + 30) + 10 + ((parent_box_size - box.height) / 2) + mouse_wheel, box.width, box.height}))
 					collision = 1;
 				i++;
 			}
-
 			set_cursor((t_mouse_state) {collision, MOUSE_CURSOR_POINTING_HAND, MOUSE_CURSOR_ARROW});
 
 			i = 0;
 			while (i < app_size)
 			{
 				color =  button_state_color(0);
-				if (CheckCollisionPointRec(GetMousePosition(), (Rectangle){screen_dimention.width - box.width - 10, i * (parent_box_size + 30) + 10 + ((parent_box_size - box.height) / 2), box.width, box.height}))
+				if (CheckCollisionPointRec(GetMousePosition(), (Rectangle){screen_dimention.width - box.width - 10, i * (parent_box_size + 30) + 10 + ((parent_box_size - box.height) / 2) + mouse_wheel, box.width, box.height}))
 					color = GetColor(0x70e000FF);
 
 				//IMPORTANT
-				DrawRectangleRounded((Rectangle){screen_dimention.width - box.width - 10, i * (parent_box_size + 30) + 10 + ((parent_box_size - box.height) / 2), box.width, box.height}, 0.1f, 1, color);
+				DrawRectangleRounded((Rectangle){screen_dimention.width - box.width - 10, i * (parent_box_size + 30) + 10 + ((parent_box_size - box.height) / 2) + mouse_wheel, box.width, box.height}, 0.1f, 1, color);
 
 				//TEXT: INSTALL REMOVE INSTALLED REMOVED
-				DrawText("install", screen_dimention.width - box.width - 10 + (box.width - (MeasureText("install", 15))) / 2, i * (parent_box_size + 30) + 10 + (parent_box_size - 15) / 2, 15, BLACK);
+				DrawText("install", screen_dimention.width - box.width - 10 + (box.width - (MeasureText("install", 15))) / 2, i * (parent_box_size + 30) + 10 + (parent_box_size - 15) / 2 + mouse_wheel, 15, BLACK);
 
-				DrawTexture(sprite[i], 10, i * parent_box_size + (parent_box_size - 64)/2 + 10 + i * 30, WHITE);
-				DrawText(str[i], 20 + 64, i * (parent_box_size + 30) + 20, 20, WHITE);
+				DrawTexture(sprite[i], 10, i * parent_box_size + (parent_box_size - 64)/2 + 10 + i * 30 + mouse_wheel, WHITE);
+				DrawText(str[i], 20 + 64, i * (parent_box_size + 30) + 20 + mouse_wheel, 20, WHITE);
 				i++;
 			}
 		}
